b44b6e989b8a4dd77fe310de87b53da3dc833d51
praparn <eva10409@gmail.com>
2018-02-02 00:17:50 +0700

    20180202001749

--- WorkShop_2.5_Kubernetes_RealWorld/initialsetup.sh
+++ WorkShop_2.5_Kubernetes_RealWorld/initialsetup.sh
@@ -21,7 +21,7 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 #Install Kubernetes Base
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.9.2/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 sudo apt-get update && apt-get install -y apt-transport-https
@@ -30,4 +30,4 @@ sudo touch /etc/apt/sources.list.d/kubernetes.list
 #sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial-1.7 main" > /etc/apt/sources.list.d/kubernetes.list'
 sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
 sudo apt-get update
-sudo apt-get install -y kubelet=1.7.0-00 kubeadm=1.7.0-00
+sudo apt-get install -y kubelet=1.9.2-00 kubeadm=1.9.2-00
